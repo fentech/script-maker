e5988261d71602287993d02fe1cdc4f57a3587e5
Joe Fensler <joe.fensler@gmail.com>
2019-12-13 19:58:12 -0600

    feat: add index

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import capitalize from './utils/capitalize/capitalize';
+import compose from './utils/compose/compose';
+import {
+    boldConsoleText,
+    greenConsoleText,
+    redConsoleText,
+    yellowConsoleText,
+} from './utils/consoleFormat/consoleFormat';
+import createFile from './utils/createFile/createFile';
+import escapeQuotes from './utils/escapeQuotes/escapeQuotes';
+import executeCmd from './utils/executeCmd/executeCmd';
+import executeIfFileExists from './utils/executeIfFileExists/executeIfFileExists';
+import executeUnlessFileExists from './utils/executeUnlessFileExists/executeUnlessFileExists';
+import log from './utils/log/log';
+import stringify from './utils/stringify/stringify';
+import warn from './utils/warn/warn';
+
+export {
+    boldConsoleText,
+    capitalize,
+    compose,
+    createFile,
+    escapeQuotes,
+    executeCmd,
+    executeIfFileExists,
+    executeUnlessFileExists,
+    greenConsoleText,
+    log,
+    redConsoleText,
+    stringify,
+    warn,
+    yellowConsoleText,
+};
